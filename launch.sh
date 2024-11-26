0a453801f285cce33cb7096c5cd515a66cd70c4e
Liren Tu <tuliren@gmail.com>
2024-11-26 02:29:16 -0800

    Update stack and model version

--- launch.sh
+++ launch.sh
@@ -50,8 +50,8 @@ set_and_export_env_var "JWT_SECRET" "$(generate_random_string 64)"
 set_and_export_env_var "API_SECRET" "$(generate_random_string 64)"
 set_and_export_env_var "AUTH_SECRET" "$(generate_random_string 64)"
 
-set_and_export_env_var "STACK_VERSION" "df7136eba0"
-set_and_export_env_var "MODEL_IMAGE" "liquidai/lfm-3b:0.0.2"
+set_and_export_env_var "STACK_VERSION" "2b3f969864"
+set_and_export_env_var "MODEL_IMAGE" "liquidai/lfm-3be:0.0.6"
 
 MODEL_NAME=Liquid-$(extract_model_name "$MODEL_IMAGE")
 set_and_export_env_var "MODEL_NAME" "$MODEL_NAME" true

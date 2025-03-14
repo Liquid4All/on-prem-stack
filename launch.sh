8fa9dc0ae445f370d4e8a73fb8f93fdcecf2d60d
Liren Tu <tuliren@gmail.com>
2025-03-13 22:09:55 -0700

    Use lfm prefix

--- launch.sh
+++ launch.sh
@@ -53,7 +53,7 @@ set_and_export_env_var "AUTH_SECRET" "$(generate_random_string 64)"
 set_and_export_env_var "STACK_VERSION" "2685ff757d-0312"
 set_and_export_env_var "MODEL_IMAGE" "liquidai/lfm-1b-e:0.0.1"
 
-MODEL_NAME=Liquid-$(extract_model_name "$MODEL_IMAGE")
+MODEL_NAME=lfm-$(extract_model_name "$MODEL_IMAGE")
 set_and_export_env_var "MODEL_NAME" "$MODEL_NAME" true
 
 set_and_export_env_var "POSTGRES_DB" "liquid_labs"
